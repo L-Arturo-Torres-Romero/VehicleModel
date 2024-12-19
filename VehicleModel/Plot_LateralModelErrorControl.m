cc6607298145a45ebab5256bf7501e5da5f5ba78
Luis Arturo Torres Romero <54611814+L-Arturo-Torres-Romero@users.noreply.github.com>
2024-12-19 16:51:58 -0600

    small fix for paper
    small fix for paper

--- VehicleModel/Plot_LateralModelErrorControl.m
+++ VehicleModel/Plot_LateralModelErrorControl.m
@@ -1,6 +1,12 @@
+figure;
+set(gcf, 'Color', 'w'); % Set the figure background color to white
 P = racetrackwaypoints(:,1:2);
-plot(P(:,1),P(:,2),'r');
+plot(P(:,1),P(:,2),'r', 'LineStyle', '-');
 hold on;
 J = VehicleXY;
-plot(J(:,1),J(:,2),'b');
+plot(J(:,1),J(:,2),'b', 'LineStyle', '--');
 hold off;
+
+% Add labels to the axes
+xlabel('X axis (m)', 'FontSize', 12);
+ylabel('Y axis (m)', 'FontSize', 12);
