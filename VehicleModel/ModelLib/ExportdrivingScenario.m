867efe297f592e394206014f46976f9b6a078911
Luis Arturo Torres Romero <54611814+L-Arturo-Torres-Romero@users.noreply.github.com>
2025-04-05 13:54:23 -0600

    Update ExportdrivingScenario.m

--- VehicleModel/ModelLib/ExportdrivingScenario.m
+++ VehicleModel/ModelLib/ExportdrivingScenario.m
@@ -1,32 +1,21 @@
-
-load('waypoints.mat'); % Debe contener una variable con la lista de waypoints
-waypoints = racetrackwaypoints(:, 1:2); % Solo X, Y
-velocities = racetrackwaypoints(:, 3); % Velocidad en cada punto
+load('waypoints.mat'); % It must contain a variable with the way-point list.
+waypoints = racetrackwaypoints(:, 1:2); % Only X, Y
+velocities = racetrackwaypoints(:, 3); % Velocity on each point.
 
 realVehiclePose = VehiclePose;
 
-
-% Crear el escenario
+% Create the scenario.
 scenario = drivingScenario('SampleTime',0.1','StopTime',60);
 
-roadCenters = [waypoints, zeros(size(waypoints(:,1)))]; % Usa los waypoints cargados
-% Agregar road center al escenario
+roadCenters = [waypoints, zeros(size(waypoints(:,1)))]; % Use the loaded way-points
+% Add the read center to the scenario. 
 cr = road(scenario, roadCenters, 'Lanes', lanespec(2)); % Define lanes explicitly
-%cr = road(scenario,roadCenters,6);
 
-
-% Agregar un vehículo
-veh = vehicle(scenario, ClassID=1,PlotColor='red',Name='EgoMamalon');
+% Add a vehicle.
+veh = vehicle(scenario, ClassID=1,PlotColor='red',Name='EgoVehicle');
 %roadActor = actor(scenario, 'ClassID', 3, 'Position', [0,0,0]); % Static road object
 
-
-%Definir la trayectoria del vehículo con los waypoints y velocidades
+% Define the vehicle trajectory with the way-points and speeds.
 smoothTrajectory(veh, realVehiclePose);
 
-
 drivingScenarioDesigner(scenario); %this is to open the drivingScenarioDesigner
-
-
-
-
-
