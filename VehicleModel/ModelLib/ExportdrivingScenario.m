c33672f8d956fffeaf77e49ecc1034e4d8d3ce79
Luis Arturo Torres Romero <luisarturo.torres7@gmail.com>
2025-05-24 13:37:09 -0600

    project installer updated
    project installer updated

--- VehicleModel/ModelLib/ExportdrivingScenario.m
+++ VehicleModel/ModelLib/ExportdrivingScenario.m
@@ -2,7 +2,7 @@ load('waypoints.mat'); % It must contain a variable with the way-point list.
 waypoints = racetrackwaypoints(:, 1:2); % Only X, Y
 velocities = racetrackwaypoints(:, 3); % Velocity on each point.
 
-realVehiclePose = VehiclePose;
+realVehiclePose = out.VehiclePose;
 
 % Create the scenario.
 scenario = drivingScenario('SampleTime',0.1','StopTime',60);
